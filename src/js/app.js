2c6b679e93b3f20a3915c6b60a490059a4969d1f
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-10-19 19:11:43 +0300

    dragg added

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,9 @@ const progressBtn = document.querySelector('[data-add="progress"]');
 const doneBtn = document.querySelector('[data-add="done"]');
 const boxes = document.querySelectorAll('.box');
 const containers = document.querySelectorAll('.task-container');
+const con = document.querySelector('.container');
 let draggedItem;
+let sortingItem;
 
 boxes.forEach(item => item.addEventListener('mouseover', showCloseMark));
 boxes.forEach(item => item.addEventListener('mouseleave', hideCloseMark));
@@ -24,17 +26,29 @@ close.forEach(item => item.addEventListener('click', closeForm));
 addBtns.forEach(item => item.addEventListener('click', getTask));
 
 containers.forEach(item => item.addEventListener('mousedown', dragg));
+//con.addEventListener('mousedown', dragg);
 
 function onDrag(e) {
     console.log(e);
+    draggedItem.style.top = `${e.layerY}px`;
+    draggedItem.style.left = `${e.layerX}px`;
 }
 
 function dragg(e) {
-    draggedItem = e.target;
-    if (!draggedItem.hasAttribute('data-task')) return;
+    draggedItem = e.target.cloneNode(true);
+    sortingItem = e.target;
+    if (!sortingItem.hasAttribute('data-task')) return;
     draggedItem.classList.add('dragged');
-    const container = draggedItem.closest('.task-container');
+    const container = sortingItem.closest('.container');
     container.addEventListener('mousemove', onDrag);
-    
+    container.addEventListener('mouseup', (e) => {
+        const element = document.elementFromPoint(e.clientX, e.clientY);
+        container.insertBefore(sortingItem, element);
+        draggedItem.classList.remove('dragged');
+        draggedItem.style.top = undefined;
+        draggedItem.style.left = undefined;
+        //draggedItem = undefined;
+        container.removeEventListener('mousemove', onDrag);
+    })    
     console.log(e.target);
 }
